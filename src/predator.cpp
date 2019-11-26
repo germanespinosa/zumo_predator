a3b7f996c22d34ca4ab8f99fe0b7dbac8e8e8803
German Espinosa <germanespinosa@gmail.com>
2019-11-26 16:00:29 +0000

    modified:   src/predator.cpp

--- src/predator.cpp
+++ src/predator.cpp
@@ -8,38 +8,31 @@ using namespace std;
 using namespace gcomm;
 
 struct data {
-    int16_t v[3];
+    int16_t state, speed, angle;
 };
 
 int main(int argc, char *argv[])
 {
     GUartBuffers serial(GUart::Baudrate::b57600);
     GBufferConnector conn(serial.rx_buffer, serial.tx_buffer);
-    GComm<data,data> comm(conn);
     Prey_finder pf;
     double angle,size;
     bool active = true;
-    data d;
     while(active) {
+        data d {0,0,0};
         double age = pf.get_prey(angle, size);
         if (age<3){
             if (size>6000) {
-                d.v[0] = 2; // caught
-                d.v[1] = 0;
+                d.state = 2; // caught
             } else {
-                d.v[0] = 1; //found
-                d.v[1] = 0;
-                d.v[2] = angle;
+                d.state = 1; //found
+                d.speed = 100;
+                d.angle = angle;
             }
-        } else {
-            d.v[0] = 0; // lost contact
-            d.v[1] = 0;
-            d.v[2] = 0;
-        }
-        for (int i=0;i<3;i++)
-            cout << (int) d.v[i] << " ";
+        } // lost contact
+        cout << d.state << " " << d.speed << " " << d.angle << " ";
         cout << endl;
-        comm.write_object(d);
-        usleep(1000000);
+        conn.write_buffer(&d, sizeof(data));
+        usleep(100000);
     }
 }
